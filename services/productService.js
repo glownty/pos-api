decb19e162f7d0cd685df065529131eb1d3f9f61
glownt <glownt.com@gmail.com>
2026-04-25 23:03:41 -0300

    fix products bugs

--- services/productService.js
+++ services/productService.js
@@ -1,4 +1,7 @@
-﻿exports.createProduct = async (data) => {
+﻿const AppError = require("../utils/AppError");
+const productRepo = require("../repositories/productRepository");
+
+exports.createProduct = async (data) => {
     let {
         userId,
         name,
@@ -34,3 +37,76 @@
 
     return { msg: 'Created product' };
 };
+
+exports.getAllProducts = async (userId) => {
+    return productRepo.getAllProducts(userId);
+}
+exports.getProductById = async (id, userId) => {
+    return productRepo.getProductById(id, userId);
+}
+exports.getProductByBarcode = async (barcode, userId) => {
+    return productRepo.getProductByBarcode(barcode, userId);
+}
+exports.getProductByName = async (name, userId) => {
+    if (!name || !name.trim()) {
+        return [];
+    }
+    if (!userId) {
+        throw new AppError('userId is invalid');
+    }
+    const clean = name.trim();
+
+    if (clean.length > 50) {
+        throw new AppError('search too long');
+    }
+    if (clean.length < 2) {
+        return [];
+    }
+    return productRepo.getProductByName(name, userId);
+}
+exports.updateProduct = async ({
+                                   id,
+                                   userId,
+                                   name,
+                                   barcode,
+                                   price,
+                                   cost,
+                                   stock,
+                                   categoryId,
+                                   isActive
+                               }) => {
+
+    if (!name || price == null) {
+        throw new AppError('name and price is required');
+    }
+
+    if (barcode) {
+        const existing = await productRepo.getProductByBarcode(barcode, userId);
+
+        if (existing.length && existing[0].id != id) {
+            throw new AppError('barcode already exists');
+        }
+    }
+
+    await productRepo.updateProduct(
+        id,
+        userId,
+        name,
+        barcode,
+        price,
+        cost,
+        stock,
+        categoryId,
+        isActive
+    );
+
+    return await productRepo.getAllProducts(userId);
+};
+exports.deleteProduct = async (id, userId) => {
+    const product = await productRepo.getProductById(id, userId)
+    if(!product){
+        throw new AppError('product not found');
+    }
+    await productRepo.deleteProduct(id, userId);
+    return productRepo.getAllProducts();
+}
